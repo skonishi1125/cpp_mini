491ae6a216f03e7711616403e90a5be5574ce440
SKONI_NOTE\watas <skonishi1125@gmail.com>
2026-08-18 21:54:16 +0900

    前方宣言のコメント調整

--- SampleC/Core/Studies/SmartModelTest.h
+++ SampleC/Core/Studies/SmartModelTest.h
@@ -2,8 +2,8 @@
 
 #include <memory>
 
-// 前方宣言でも namespace が必要
-// SModern 名前空間の中にある Entity として宣言。呼ぶときは、SModern::Entity として呼ぶ
+// 前方宣言 指定の名前空間 SModern にある Entity を使うと宣言。
+// 呼ぶときは、SModern::Entity として呼ぶ
 namespace SModern
 {
 	class Entity;
@@ -13,7 +13,7 @@ namespace SmartModelTest
 {
 	// 以下のように書くと、SModern:: という表記を省略できるが、.h においてはこれは行わない
 	// .h を include した全てのファイルにおいても、この設定が適用されることになり、
-	// Entity と書かれたものがどの Entity なのか分からなくなる
+	// Entity と書かれたものがどの Entity なのか分からなくなるため。
 	//
 	// using namespace SModern;
 
